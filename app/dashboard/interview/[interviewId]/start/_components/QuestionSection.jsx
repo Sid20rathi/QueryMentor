fd702483ca105347b989649a3dfa73592a6bfd01
Siddhant Rathi <you@sid20rathi@gmail.com>
2024-12-28 02:09:09 +0530

    new feature

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -1,20 +1,29 @@
-import { Lightbulb } from 'lucide-react'
+import { Lightbulb, Volume2 } from 'lucide-react'
 import React, { useState } from 'react'
 
 function QuestionSection({mockInterviewQuestions}) {
     const[activeQuestion,setActiveQuestion]= useState(0);
+    const textToSpeech = (text) => {
+      if('speechSynthesis'in window) {
+        const speech = new SpeechSynthesisUtterance(text);
+        window.speechSynthesis.speak(speech);
+      }else{
+        alert('Your browser does not support text to speech feature');
+      }
+    }
 
 
   return mockInterviewQuestions&&(
     <div className='p-5 border rounded-lg my-5 shadow-xl  '>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {mockInterviewQuestions&& mockInterviewQuestions?.map((questions,index)=>(
-            <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestion===index&&'bg-purple-600 text-white'}`} onClick={()=> setActiveQuestion(index)}>Question #{index+1}</h2>
+            <h2 className={`p-2 bg-secondary rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestion===index&& 'bg-purple-600 text-white'}`} onClick={()=> setActiveQuestion(index)}>Question #{index+1}</h2>
         ))}
         
         </div>
         <div className='h-[200px] items-center mt-10'>
-        <h2 className='my-5 text-lg md:text-md'>{mockInterviewQuestions[activeQuestion]?.question}</h2> 
+        <h2 className='my-5 text-lg md:text-md'>{mockInterviewQuestions[activeQuestion]?.question}</h2>
+        <Volume2 onClick={()=>textToSpeech(mockInterviewQuestions[activeQuestion]?.question)} className='cursor-pointer text-purple-600'/> 
 
         </div>
         
--- app/dashboard/interview/[interviewId]/start/_components/VideoSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/VideoSection.jsx
@@ -1,23 +1,54 @@
-import { WebcamIcon } from 'lucide-react'
-import React, { useState } from 'react'
+"use client"
+import useSpeechToText from 'react-hook-speech-to-text';  
+
+import { Button } from '@/components/ui/button'
+import { Mic, WebcamIcon } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
 import Webcam from 'react-webcam'
 
 function VideoSection() {
-    const [webCamEnable, setWebCamEnable] = useState(false)
+  const [userAnswer, setUserAnswer] = useState('')
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false
+  });
+  useEffect(()=>{
+    results.map((result)=>(
+      setUserAnswer(prevAnswer => prevAnswer+result?.transcript)
+
+    ))
+
+  },[results])
+    const [webCamEnable, setWebCamEnable] = useState(true)
   return (
-    <div className="mt-10 pl-12 h-[500px] flex justify-center flex-col items-center ">
-    <div className="h-[500px] w-[500px] flex items-center justify-center  ">
-      {webCamEnable ? (
-        <Webcam 
-          height={500} 
-          width={500} 
-          mirrored={true} 
-          className="rounded-lg" 
-        />
-      ) : (
-        <WebcamIcon className="h-64 w-64 m-4" />
-      )}
-    </div>
+    <div className='flex flex-col items-center justify-center '>
+          <div className="mt-10 pl-12 h-[500px] flex justify-center flex-col items-center ">
+          <div className="h-[500px] w-[500px] flex items-center justify-center  ">
+            {webCamEnable ? (
+              <Webcam 
+                height={500} 
+                width={500} 
+                mirrored={true} 
+                className="rounded-lg" 
+              />
+            ) : (
+              <WebcamIcon className="h-64 w-64 m-4" />
+            )}
+          </div>
+        </div>
+        <Button variant="outline" className="ml-16 mb-10" onClick={isRecording?stopSpeechToText:startSpeechToText}>
+          {isRecording? <h2 className='text-red-600 flex gap-2'><Mic/>Stop Recording</h2>
+             : 'Record Answer'}</Button>
+
+       <Button onClick={()=>console.log(userAnswer)}>Your answer</Button>
+        
   </div>
   
   )
